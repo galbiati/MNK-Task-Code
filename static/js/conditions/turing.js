feba63508b700460662baf3152348a1d123779b7
Gianni <galbiatig@gmail.com>
2016-09-13 14:03:10 -0400

    turing update

--- static/js/conditions/turing.js
+++ static/js/conditions/turing.js
@@ -1,13 +1,10 @@
 // Todos
-//   Play button
 //   Slider + submit button
-//   No controls
-//   Feedback
 //   No early choices
-//   
 
 var clip, trial_start
 var clip_files = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+var clip_answers = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
 
 var stim_source = $('#stim-source') //document.getElementById('stim-source');
 var player = document.getElementById('turing-stim');
@@ -18,8 +15,8 @@ $(document).ready(function() {
     initPlayer();
     loadVideo(clip);
     trial_start = Date.now();
-    player.play() // probably want custom function for playing, to add callbacks etc
-    $('.response-btn').on('click', function(e) { buttonHandler(e); }).prop('disabled', false);
+    $('.response-btn').on('click', function(e) { responseHandler(e); }).prop('disabled', false);
+    $('.play-btn').on('click', function(e) { playHandler(e); })
 })
 
 function initPlayer() {
@@ -30,7 +27,13 @@ function loadVideo(clipno) {
     // add optional callback
     stim_source.attr('src', getClip(clipno));
     player.load();
-    //
+    $('.play-btn').prop('disabled', false);
+}
+
+function playHandler(e){
+    player.play();
+    $('.play-btn').prop('disabled', true);
+    $('#feedbacktext').text("");
 }
 
 function submit_response(val) {
@@ -44,9 +47,12 @@ function submit_response(val) {
     return $.ajax({type: 'POST', url: '/turing', dataType:'JSON', data:response})
 }
 
-function buttonHandler(e) {
+function responseHandler(e) {
     player.pause();
     var val = (e.target.id) ? e.target.id : $(e.target).parent().attr("id");
+    val = (val=="human")? 1:0;
+    feedback = (val == clip_answers[i]);
+    $('#feedbacktext').text(String(feedback));
     res = submit_response(val);
     res.done(console.log('Data sent!'));
     i ++ ;
@@ -55,9 +61,9 @@ function buttonHandler(e) {
     }
     $('.response-btn').prop('disabled', true);
     clip = clip_files[i];
-    loadVideo(clip)
-    player.play();
+    loadVideo(clip);
     $('.response-btn').prop('disabled', false);
+
 }
 
 function getClip(clipno) {
